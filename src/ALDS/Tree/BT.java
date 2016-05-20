a2c9298de2b605058ee1231972ee1dcdbcbc976a
zimingwang <ziming.wang@west.cmu.edu>
2016-05-19 19:23:34 -0700

    7

--- src/ALDS/Tree/BT.java
+++ src/ALDS/Tree/BT.java
@@ -1,8 +1,44 @@
 package ALDS.Tree;
 
+import JavaCode.ObjectSizeCalculator;
+
 /**
  * Created by wangziming on 5/19/16.
  */
+
+interface BTS<T>{
+
+    public T getVal();
+    public BTS getRight();
+    public BTS getLeft();
+}
+
+class LeafNode<T> implements BTS{
+
+    public T val;
+
+    public LeafNode(T val){
+        this.val = val;
+    }
+
+    @Override
+    public Object getVal() {
+        return val;
+    }
+
+    @Override
+    public BTS getRight() {
+        return null;
+    }
+
+    @Override
+    public BTS getLeft() {
+        return null;
+    }
+}
+
+
+
 public class BT<T> {
     public   T val;
     public  BT right;
@@ -16,7 +52,7 @@ public class BT<T> {
 
 class GeneratedTree{
 
-    public static void main(String[] argv){
+    public static void main(String[] argv) throws IllegalAccessException {
         BT root = new BT(3);
         BT left = new BT("ss");
         BT right = new BT("sddaf");
@@ -24,13 +60,19 @@ class GeneratedTree{
         root.left = left;
         root.right = right;
 
+        pre_traverse(root);
+        System.out.println(ObjectSizeCalculator.sizeOf(new BT("sdsds")));
+        System.out.println(ObjectSizeCalculator.sizeOf(new LeafNode("sdsdsd")));
     }
 
 
-    public static void traverse(BT root){
+    public static void pre_traverse(BT root){
         if(root == null){
-
+            return;
         }
+        System.out.println(root.val);
+        pre_traverse(root.left);
+        pre_traverse(root.right);
     }
 
 
